100c3a58652001948fc7fd3b824d701d61263d2f
Victor <emmyvic98@gmail.com>
2024-04-23 05:38:45 +0100

    add course

--- src/course/course.dto.ts
+++ src/course/course.dto.ts
@@ -1,6 +1,12 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNotEmpty, IsNumber, IsString } from 'class-validator';
 
 export class CourseDto {
+  @IsNumber()
+  @IsNotEmpty()
+  @Type(() => Number)
+  termId: number;
+
   @IsString()
   @IsNotEmpty()
   title: string;
