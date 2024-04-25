0872bf3c16dcca1b106fda0dfb0afb094bd36b87
Victor <emmyvic98@gmail.com>
2024-04-26 00:12:25 +0100

    add services for task module

--- src/task/task.module.ts
+++ src/task/task.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { TaskController } from './task.controller';
 import { TaskService } from './task.service';
+import { UserService } from 'src/user/user.service';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Module({
   controllers: [TaskController],
-  providers: [TaskService]
+  providers: [TaskService, UserService, PrismaService],
 })
 export class TaskModule {}
